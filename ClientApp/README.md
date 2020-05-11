c97fbd11281914fc2ab7e13f9b237325ae9bcaea
yitzolsberg <yitzolsberg@gmail.com>
2020-05-11 12:37:55 +0100

    Updated to new vue

--- ClientApp/README.md
+++ ClientApp/README.md
@@ -1,4 +1,4 @@
-# ASP.NET Core Vue Starter
+# client-app
 
 ## Project setup
 ```
@@ -15,7 +15,5 @@ npm run serve
 npm run build
 ```
 
-### Lints and fixes files
-```
-npm run lint
-```
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- ClientApp/babel.config.js
+++ ClientApp/babel.config.js
@@ -1,21 +1,5 @@
 module.exports = {
-  "presets": [
-    [
-      "@vue/cli-plugin-babel/preset",
-      {
-        "useBuiltIns": "entry"
-      }
-    ]
-  ],
-  "plugins": [
-    [
-      "transform-imports",
-      {
-        "vuetify": {
-          "transform": "vuetify/es5/components/${member}",
-          "preventFullImport": true
-        }
-      }
-    ]
+  presets: [
+    '@vue/cli-plugin-babel/preset'
   ]
 }
